03b00e6ec64605213cebb399ecabd1715df9d43b
Dominik Nussbaumer <nussbaumer.dominik@pm.me>
2023-09-05 15:44:10 +0200

    added rgba colors and modularized tag creation

--- a3/src/Syntax.hs
+++ a3/src/Syntax.hs
@@ -5,6 +5,7 @@ module Syntax where
 
 import qualified GI.Gtk as Gtk
 import qualified Data.Text as T
+import GI.Gdk.Structs.RGBA
 import Data.GI.Base
 
 data SyntaxRule = SyntaxRule
@@ -24,20 +25,42 @@ variablePattern = T.concat ["(?<!\\w)(?!(?:" , keywordsPattern , "))(\\w+)(?!\\[
 valuePattern :: T.Text
 valuePattern = "\\b[0-9]+\\b"
 
+commentPattern :: T.Text
+commentPattern = "--[^\n]*$" -- everything after -- until end of line
+
 syntaxRules :: [SyntaxRule]
 syntaxRules = [ SyntaxRule keywordsPattern  "keyword"
               , SyntaxRule valuePattern     "value"
               , SyntaxRule variablePattern  "variable"
+              , SyntaxRule commentPattern   "comment"
               ]
 
 initTagTable :: Gtk.TextTagTable -> IO ()
 initTagTable tagTable = do
-    keywordTag <- Gtk.new Gtk.TextTag [#name := "keyword", #foreground := "blue"]
-    _ <- Gtk.textTagTableAdd tagTable keywordTag
-
-    variableTag <- Gtk.new Gtk.TextTag [#name := "variable", #foreground := "pink"]
-    _ <- Gtk.textTagTableAdd tagTable variableTag
-
-    valueTag <- Gtk.new Gtk.TextTag [#name := "value", #foreground := "orange"]
-    _ <- Gtk.textTagTableAdd tagTable valueTag
+    _ <- createAndAddTag "keyword" 0 51 179 1 tagTable
+    _ <- createAndAddTag "variable" 0 0 0 1 tagTable
+    _ <- createAndAddTag "value" 42 172 184 1 tagTable
+    _ <- createAndAddTag "comment" 140 140 140 1 tagTable
     return ()
+
+createTag :: T.Text -> Int -> Int -> Int -> Int -> IO Gtk.TextTag
+createTag tagName r g b a = do
+    color <- newGdkRGBA r g b a
+    Gtk.new Gtk.TextTag [#name := tagName, #foregroundRgba := color]
+
+addTag :: Gtk.TextTagTable -> Gtk.TextTag -> IO ()
+addTag tagTable tag = do
+    _ <- Gtk.textTagTableAdd tagTable tag
+    return ()
+
+createAndAddTag :: T.Text -> Int -> Int -> Int -> Int -> Gtk.TextTagTable -> IO Gtk.TextTag
+createAndAddTag tagName r g b a tagTable =
+    createTag tagName r g b a >>= \tag -> addTag tagTable tag >> return tag
+
+newGdkRGBA :: Int -> Int -> Int -> Int -> IO RGBA
+newGdkRGBA r g b a = Gtk.new RGBA [ #red := fromIntegral r / 255
+                                  , #green := fromIntegral g / 255
+                                  , #blue := fromIntegral b / 255
+                                  , #alpha := fromIntegral a
+                                  ]
+
