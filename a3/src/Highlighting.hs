6d84954d6e5ab4120cbe8b343770620a34c83f41
Dominik Nussbaumer <nussbaumer.dominik@pm.me>
2023-09-05 12:41:15 +0200

    improved syntax highlighting so variables, keywords and values are correctly recognized on the first load

--- a3/src/Highlighting.hs
+++ a3/src/Highlighting.hs
@@ -7,8 +7,8 @@ module Highlighting
 
 import qualified Data.Text as T
 import qualified GI.Gtk as Gtk
-import Text.Megaparsec
 import Control.Monad (forM_)
+import Text.Regex.PCRE
 import Syntax
 
 highlightSyntax :: Gtk.TextBuffer -> IO ()
@@ -33,61 +33,17 @@ highlightSyntax buffer = do
             Nothing -> return ()
     return ()
 
--- helper functions
---getTokenPositions :: T.Text -> [SyntaxRule] -> [(String, (Int, Int))]
---getTokenPositions text rules =
---concatMap (findMatches text) rules
---where
---    findMatches :: T.Text -> SyntaxRule -> [(String, (Int, Int))]
---    findMatches t rule = case parse (manyTill parserWithPos eof) "" t of
---        Left _ -> []
---        Right matches -> matches
---      where
---        parserWithPos = do
---          startPos <- getSourcePos
---          _ <- parser rule
---          endPos <- getSourcePos
---          let style = styleTag rule
---              startOffset = sourcePosToOffset t startPos
---              endOffset = sourcePosToOffset t endPos
---          return (style, (startOffset, endOffset))
-
 getTokenPositions :: T.Text -> [SyntaxRule] -> [(String, (Int, Int))]
-getTokenPositions text rules = go text 0
-    where
-        go :: T.Text -> Int -> [(String, (Int, Int))]
-        go t offset
-            | T.null t = []
-            | otherwise =
-                let matches = concatMap (findMatch t offset) rules
-                in case matches of
-                     []     -> go (T.drop 1 t) (offset + 1)  -- No match, skip one character
-                     (m:_)  -> m : go (T.drop (snd (snd m) - offset + 1) t) (snd (snd m))  -- Match found, jump to the end of the match
+getTokenPositions text rules =
+    concatMap (\rule -> findMatches text rule) rules
 
-        findMatch :: T.Text -> Int -> SyntaxRule -> [(String, (Int, Int))]
-        findMatch t offset rule = case parse (parserWithPos rule) "" t of
-            Left _       -> []
-            Right match  -> [(styleTag rule, (fst match + offset, snd match + offset))]
-
-        parserWithPos :: SyntaxRule -> Parser (Int, Int)
-        parserWithPos rule = do
-            startPos <- getOffset
-            _ <- parser rule
-            endPos <- getOffset
-            return (startPos, endPos)
-
-        getOffset = length <$> manyTill anySingle (lookAhead (try anyRule <|> (eof >> return T.empty)))
-        anyRule = foldl1 (<|>) (parser <$> rules)
-
+findMatches :: T.Text -> SyntaxRule -> [(String, (Int, Int))]
+findMatches t rule =
+    let matches = getAllMatches $ T.unpack t =~ T.unpack (pattern rule) :: [(Int, Int)]
+    in map (\(start, len) -> (styleTag rule, (start, start + len))) matches
 
 getTag :: Gtk.TextTagTable -> String -> IO (Maybe Gtk.TextTag)
 getTag tagTable name = Gtk.textTagTableLookup tagTable (T.pack name)
 
 getIterAtOffset :: Gtk.TextBuffer -> Int -> IO Gtk.TextIter
 getIterAtOffset buffer offset = Gtk.textBufferGetIterAtOffset buffer (fromIntegral offset)
-
-sourcePosToOffset :: T.Text -> SourcePos -> Int
-sourcePosToOffset txt pos =
-    let precedingLines = take (unPos (sourceLine pos) - 1) (T.lines txt)
-        precedingChars = sum $ map T.length precedingLines
-    in precedingChars + (unPos (sourceColumn pos) - 1) + length precedingLines -- Add length for newline chars
--- a3/src/Syntax.hs
+++ a3/src/Syntax.hs
@@ -4,86 +4,31 @@
 module Syntax where
 
 import qualified GI.Gtk as Gtk
-import Text.Megaparsec
-import Text.Megaparsec.Char
-import Text.Megaparsec.Char.Lexer
-import Data.Text (Text, unpack, pack)
-import Data.Void (Void)
-import Data.List (nub)
-import Data.Char (isSpace, isPunctuation, isSymbol, isLetter)
+import qualified Data.Text as T
 import Data.GI.Base
 
-type Parser = Parsec Void Text
-
 data SyntaxRule = SyntaxRule
-    { parser  :: Parser Text
+    { pattern  :: T.Text
     , styleTag :: String
     }
 
-keywords :: [Text]
+keywords :: [T.Text]
 keywords = ["plus", "minus", "mult", "div", "cont", "each"]
 
-identifierChar :: Parser Char
-identifierChar = letterChar <|> char '_'
+keywordsPattern :: T.Text
+keywordsPattern = T.concat ["(?<!\\w)(" , T.intercalate "|" keywords , ")(?!\\w)"]
 
---keywordParser :: Parser Text
---keywordParser = skipMany (noneOfKeywordStarts) >> choice (map string keywords) <* notFollowedBy alphaNumChar
+variablePattern :: T.Text
+variablePattern = T.concat ["(?<!\\w)(?!(?:" , keywordsPattern , "))(\\w+)(?!\\[\\(\\{\\w])"]
 
---keywordParser :: Parser Text
---keywordParser = choice (string <$> keywords) <* notFollowedBy alphaNumChar
-
-keywordParser :: Parser Text
-keywordParser = try $ do
-    kw <- choice (string <$> keywords)
-    notFollowedBy identifierChar
-    return kw
-
-
---noneOfKeywordStarts :: Parser Char
---noneOfKeywordStarts = noneOf $ nub (map (head . unpack) keywords)
-
--- every word that isn't a keyword and isn't followed by a bracket
---variableParser :: Parser Text
---variableParser = try $ do
---    -- Parse a sequence of non-space characters
---    v <- takeWhile1P (Just "non-keyword") (\ch -> not (isSpace ch) && ch /= '(')
---
---    -- Check if the parsed text is not one of the keywords
---    if v `elem` keywords
---        then fail "Parsed text is a keyword"
---        else do
---            -- Ensure the matched text isn't followed by an opening bracket
---            notFollowedBy (char '(')
---            return v
-
---variableParser :: Parser Text
---variableParser = try $ do
---    v <- takeWhile1P (Just "non-keyword") (\ch -> not (isSpace ch) && ch /= '(' && not (isPunctuation ch) && not (isSymbol ch))
---    if v `elem` keywords
---        then fail "Parsed text is a keyword"
---        else notFollowedBy (char '(') >> return v
-
-variableParser :: Parser Text
-variableParser = try $ do
-    v <- takeWhile1P (Just "identifier") isIdentifierChar
-    if v `elem` keywords
-        then fail "Parsed text is a keyword"
-        else return v
-  where
-    isIdentifierChar ch = isLetter ch || ch == '_'
-
-
--- Integers
-valueParser :: Parser Text
-valueParser = fmap showt decimal
-  where
-    showt :: Integer -> Text
-    showt = pack . show
+valuePattern :: T.Text
+valuePattern = "\\b[0-9]+\\b"
 
 syntaxRules :: [SyntaxRule]
-syntaxRules = [SyntaxRule keywordParser  "keyword",
-               SyntaxRule valueParser    "value",
-               SyntaxRule variableParser "variable"]
+syntaxRules = [ SyntaxRule keywordsPattern  "keyword"
+              , SyntaxRule valuePattern     "value"
+              , SyntaxRule variablePattern  "variable"
+              ]
 
 initTagTable :: Gtk.TextTagTable -> IO ()
 initTagTable tagTable = do
