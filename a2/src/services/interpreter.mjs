23c9bfb75c93fe490025a182d84a9388fd558996
Matthias Schellner <matthias@schellner.net>
2023-09-07 17:12:57 +0200

    a2: started refactoring the interpreter

--- a2/src/services/interpreter.mjs
+++ a2/src/services/interpreter.mjs
@@ -2,6 +2,24 @@ import {Basic, Value} from "../models/expressionClasses.mjs";
 
 const memory = new Map()
 
+/**
+ * Interprets a list of basic expressions
+ * @param expressions - A list of basic expressions
+ */
+function interpret(expressions) {
+    for (let expression of expressions) {
+        const result = interpretBasic(expression);
+        if (!isNaN(result)) {
+            console.log(result);
+        }
+    }
+}
+
+/**
+ * Finds the result of a name/variable that is stored in the memory
+ * @param name - The name of the variable
+ * @returns {*|undefined}
+ */
 function findResult(name) {
     if (!memory.has(name)) {
         return undefined;
@@ -10,89 +28,99 @@ function findResult(name) {
     if (entry.value) {
         return entry.value;
     }
-    return interpretExpression(entry.expression);
+    return interpretBasic(entry.expression);
 }
 
-function interpretExpression(expression, eager = false) {
+/**
+ * Interprets a basic expression
+ * @param basicExpr - A basic expression
+ * @param eager
+ * @returns {*}
+ */
+function interpretBasic(basicExpr, eager = false) {
     let value, assignment = undefined;
-    if (expression instanceof Basic) {
-        value = expression.value;
-        assignment = expression.assignment;
+    if (basicExpr instanceof Basic) {
+        value = basicExpr.value;
+        assignment = basicExpr.assignment;
     }
-    if (expression instanceof Value) {
-        value = expression;
+    if (basicExpr instanceof Value) {
+        value = basicExpr;
     }
-
 
     if (assignment) {
-        if (eager) {
-            memory.set(assignment.name, {
-                expression: assignment.expr,
-                value: interpretExpression((assignment.expr), eager)
-            })
-        } else {
-            memory.set(assignment.name, {
-                expression: assignment.expr,
-                value: undefined,
-            })
-        }
+        interpretAssignment(eager, assignment);
     }
     if (value) {
-        if (value.number !== undefined) {
-            return value.number
-        }
-        if (value.list) {
-            return value.list.map(entry => interpretExpression(entry, eager))
-        }
-        if (value.name) {
-            return findResult(value.name)
-        }
-        if (value.functionDefinition) {
-            return value.functionDefinition;
-        }
-        if (value.functionCall) {
-            const result = findResult(value.functionCall.name)
-            if (result === undefined) {
-                const builtin = builtins[value.functionCall.name];
-                if (builtin) {
-                    if(value.functionCall.name === "cond"){
-                        return builtin(value.functionCall.values);
-                    }
-                    const callValues = value.functionCall.values.map(interpretExpression)
-                    return builtin(callValues)
-                } else {
-                    throw new Error(`Function ${value.functionCall.name} not found`)
-                }
-            } else {
-                const names = result.names;
-                const values = value.functionCall.values;
-                const expressions = result.expressions;
-                for (let i = 0; i < names.length; i++) {
-                    memory.set(names[i], {
-                        expression: values[i],
-                        value: undefined,
-                    });
-                }
-                for (let expression of expressions) {
-                    interpretExpression(expression, true)
-                }
-                for (let i = 0; i < names.length; i++) {
-                    if (values[i].name) {
-                        memory.set(values[i].name, memory.get(names[i]));
-                    }
-                }
-                return result
-            }
-        }
+        return interpretValue(value, eager);
     }
 }
 
-function interpret(expressions) {
-    for (let expression of expressions) {
-        const result = interpretExpression(expression);
-        if (!isNaN(result)) {
-            console.log(result);
+function interpretAssignment(eager, assignment) {
+    if (eager) {
+        memory.set(assignment.name, {
+            expression: assignment.expr,
+            value: interpretBasic((assignment.expr), eager)
+        })
+    } else {
+        memory.set(assignment.name, {
+            expression: assignment.expr,
+            value: undefined,
+        })
+    }
+}
+
+function interpretFunctionCall(value) {
+    const result = findResult(value.functionCall.name)
+    if (result === undefined) {
+        const builtin = builtins[value.functionCall.name];
+        //check if it is a builtin function
+        if (builtin) {
+            if (value.functionCall.name === "cond") {
+                return builtin(value.functionCall.values);
+            }
+            const callValues = value.functionCall.values.map(interpretBasic)
+            return builtin(callValues)
+        } else {
+            throw new Error(`Function ${value.functionCall.name} not found`)
         }
+    } else {
+        //since it's not a builtin function, it must be a function definition
+        const names = result.names;
+        const values = value.functionCall.values;
+        const expressions = result.expressions;
+        for (let i = 0; i < names.length; i++) {
+            memory.set(names[i], {
+                expression: values[i],
+                value: undefined,
+            });
+        }
+        for (let expression of expressions) {
+            interpretBasic(expression, true)
+        }
+        for (let i = 0; i < names.length; i++) {
+            if (values[i].name) {
+                memory.set(values[i].name, memory.get(names[i]));
+            }
+        }
+        return result
+    }
+}
+
+function interpretValue(value, eager) {
+    if (value.number !== undefined) {
+        return value.number
+    }
+    if (value.list) {
+        return value.list.map(entry => interpretValue(entry, eager))
+    }
+    if (value.name) {
+        return findResult(value.name)
+    }
+    if (value.functionDefinition) {
+        return value.functionDefinition;
+    }
+    if (value.functionCall) {
+        return interpretFunctionCall(value);
     }
 }
 
@@ -114,16 +142,16 @@ const builtins = {
             });
 
             for (let expression of values[1].expressions) {
-                interpretExpression(expression, true)
+                interpretBasic(expression, true)
             }
         })
     },
     "cond": (values) => {
-        const logicResult =  interpretExpression(values[0], true)
+        const logicResult = interpretBasic(values[0], true)
         if (logicResult === 1) {
-            return interpretExpression(values[1], true)
+            return interpretBasic(values[1], true)
         } else {
-            return interpretExpression(values[2], true)
+            return interpretBasic(values[2], true)
         }
     },
     "eq": (values) => values[0] === values[1] ? 1 : 0,
