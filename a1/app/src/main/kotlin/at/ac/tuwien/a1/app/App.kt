3624712f38800d8fb6cca0e21bed526ec5529192
fabian <foberm16@gmail.com>
2023-09-15 00:54:40 +0200

    add mean testcase

--- a1/app/src/main/kotlin/at/ac/tuwien/a1/app/App.kt
+++ a1/app/src/main/kotlin/at/ac/tuwien/a1/app/App.kt
@@ -16,14 +16,25 @@ fun main() = runBlocking {
         initialRegisterValues = mapOf(
             'a' to DataEntry.StringEntry("y@x@"),
             'f' to DataEntry.StringEntry("'@@ 0 0 " +" (#3-4!- (4!4!6+!+ 4!1+ 4$4$  4!4$2!@) (1$1$) (4!4\$_1+\$@)@) 2!@" // calculate sum
-                    +" (Sum: )\"2!\" " // print sum
-                    +" .+#1-/ " // calculate avg
-                    +" (Avg: )\"2!\" " // print avg
+                    + " (Sum: )\"2!\" " // print sum
+                    + " .+#1-/ " // calculate avg
+                    + " (Avg: )\"2!\" " // print avg
                     + " 0 0 " +" (#4-4!- (4!4!7+!7!-2!*+ 4!1+ 4$4$ 4!4$2!@) (1$1$2$) (4!4\$_1+\$@)@) 2!@" // calculate sum of squared diffs
-                    +" (Squared diff sum: )\"2!\" " // print squared diff sum
-                    +"#1-.+/" //calculate variance
-                    +" (Variance: )\"\" " // print variance
+                    + " (Squared diff sum: )\"2!\" " // print squared diff sum
+                    + "#1-.+/" //calculate variance
+                    + " (Variance: )\"\" " // print variance
                 ),
+            'm' to  DataEntry.StringEntry(" 0 3! 0 0" // index value start offset
+                    + "(#5-4!- " // loop over all elements
+                    + "(3!7+!" // get next element
+                    + "(6!> (3! 4!8+!)(6!6!) (4!4\$_1+$@)@)@  6!6!1+ 6$6$6$6$" // update biggest element, index & offset
+                    + "6!6$2!@" // loop
+                    + ") ("
+                    + "6!7+! 7!7+$   7!7!7!7!7! 7$7$7$7$7$" // reorder new biggest value
+                    + "(#6-5!- (4!1+ 5!1+8+! 6!1+ 2! 6$6$6$6$ 6!6$2!@)(1$1$1$1$1$) (4!4\$_1+$@)@)@ " // restart loop with start set one higher until we iterated over all elements
+                    + ") (4!4\$_1+$@)@)  2!@" // now we have the input sorted
+                    + "(#2% (#2/~#+!)(#2/~#+! #2/~#1++! +.+2/) (4!4\$_1+$@)@)@ \"" // calculate median of sorted list
+            ),
             // Program to ask for input and execute it repeatedly
             'x' to DataEntry.StringEntry("'@@\"x@"),
             // Output welcome message
